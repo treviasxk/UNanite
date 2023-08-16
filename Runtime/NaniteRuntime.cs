ea9f79c959c767ddbd3440d71a2f6143bdc0f451
treviasxk <trevias@live.com>
2023-08-16 16:25:39 -0300

    • Suport to multi materials in multi objects. • Others fixeds.

--- Runtime/NaniteRuntime.cs
+++ Runtime/NaniteRuntime.cs
@@ -40,31 +40,40 @@ public class NaniteRuntime : MonoBehaviour{
                             if(!Nanites.ContainsKey(code)){
                                 Nanites.Add(code, new Nanite(){quality = 10, meshFilter = nanite.GetComponent<MeshFilter>(), transform = nanite.transform, LODs = new Dictionary<int, Mesh>(), childrenMeshFilter = new List<MeshFilter>(){}});
                             }else{
-                                Nanites[code].childrenMeshFilter.Add(nanite.GetComponent<MeshFilter>());
+                                int code2 = nanite.GetComponent<MeshRenderer>().sharedMaterial.GetInstanceID();
+                                if(Nanites[code].meshFilter.GetComponent<MeshRenderer>().sharedMaterial.GetInstanceID() == code2)
+                                    Nanites[code].childrenMeshFilter.Add(nanite.GetComponent<MeshFilter>());
+                                else{
+                                    if(!Nanites.ContainsKey(code2)){
+                                        Nanites.Add(code2, new Nanite(){quality = 10, meshFilter = nanite.GetComponent<MeshFilter>(), transform = nanite.transform, LODs = new Dictionary<int, Mesh>(), childrenMeshFilter = new List<MeshFilter>(){}});
+                                    }else{
+                                        Nanites[code2].childrenMeshFilter.Add(nanite.GetComponent<MeshFilter>());
+                                    }
+                                }
                             }
                         }
                 }
 
             }
             cameraPosition = Camera.main.gameObject.transform.position;
-            foreach(Nanite nanite in Nanites.Values.OrderBy(item => item.quality)){
-                float distance = Mathf.Pow(Vector3.Distance(nanite.transform.position, cameraPosition), 1);
+            foreach(var nanite in Nanites.OrderBy(item => item.Value.quality)){
+                float distance = Mathf.Pow(Vector3.Distance(nanite.Value.transform.position, cameraPosition), 1);
                 float value = (1 / MaxRender) * (distance - MinRender);
                 value = 1 - Mathf.Clamp(value, 0, 1);
                 int quality = Convert.ToInt16(value * 10);
 
-                if(!nanite.LODs.ContainsKey(quality)){
-                    CreateQuality(nanite, quality, value);
+                if(!nanite.Value.LODs.ContainsKey(quality)){
+                    CreateQuality(nanite.Value, quality, value);
                 }else
-                    ChangeQuality(nanite, quality);
+                    ChangeQuality(nanite.Key, nanite.Value, quality);
             }
         }else{
             if(!fisrt){
                 fisrt = true;
                 foreach(Nanite nanite in Nanites.Values){
-                    nanite.meshFilter.gameObject.SetActive(true);
+                    nanite.meshFilter.gameObject.GetComponent<MeshRenderer>().enabled = true;
                     foreach(MeshFilter nanite1 in nanite.childrenMeshFilter){
-                        nanite1.gameObject.SetActive(true);
+                        nanite1.gameObject.GetComponent<MeshRenderer>().enabled = true;
                     }
                 }
                 if(Unanite)
@@ -78,24 +87,25 @@ public class NaniteRuntime : MonoBehaviour{
         if(nanite.LODs.Count > 0){
             nanite.LODs.Add(quality, SimplifyMeshFilter(nanite.LODs.Values.OrderBy(item => item.vertexCount).Last(), value));
         }else
-            if(nanite.meshFilter.sharedMesh)
-                nanite.LODs.Add(10, SimplifyMeshFilter(nanite.meshFilter.sharedMesh, 10));
+            if(nanite.meshFilter.mesh){
+                nanite.LODs.Add(10, SimplifyMeshFilter(nanite.meshFilter.mesh, 1));
+            }
     }
 
-    void ChangeQuality(Nanite nanite, int quality){
-        if(Unanite){
-            var unanite = GameObject.Find(nanite.meshFilter.sharedMesh.GetInstanceID().ToString());
-            if(!unanite){
-                unanite = new GameObject(nanite.meshFilter.sharedMesh.GetInstanceID().ToString());
-                unanite.AddComponent<MeshFilter>();
-                unanite.AddComponent<MeshRenderer>();
-                unanite.transform.position = Vector3.zero;
-                unanite.transform.rotation = Quaternion.identity;
-                unanite.transform.parent = Unanite.transform;
-            }
-
+    void ChangeQuality(int instanceID, Nanite nanite, int quality){
+        if(Unanite)
+        if(Unanite.activeSelf){
             if(nanite.quality != quality){
                 nanite.quality = quality;
+                var unanite = GameObject.Find(instanceID.ToString());
+                if(!unanite){
+                    unanite = new GameObject(instanceID.ToString());
+                    unanite.AddComponent<MeshFilter>();
+                    unanite.AddComponent<MeshRenderer>();
+                    unanite.transform.position = Vector3.zero;
+                    unanite.transform.rotation = Quaternion.identity;
+                    unanite.transform.parent = Unanite.transform;
+                }
 
                 CombineInstance[] combine = new CombineInstance[nanite.childrenMeshFilter.Count + 1];
                 for(int i = 0; i < nanite.childrenMeshFilter.Count; i++){
@@ -109,19 +119,18 @@ public class NaniteRuntime : MonoBehaviour{
 
                     combine[i+1].mesh = nanite.LODs[quality];
                     combine[i+1].transform = nanite.childrenMeshFilter[i].transform.localToWorldMatrix;
-                    nanite.childrenMeshFilter[i].gameObject.SetActive(false);
+                    nanite.childrenMeshFilter[i].GetComponent<MeshRenderer>().enabled = false;
                 }
 
                 combine[0].mesh = nanite.LODs[quality];
                 combine[0].transform = nanite.meshFilter.transform.localToWorldMatrix;
-                nanite.meshFilter.gameObject.SetActive(false);
+                nanite.meshFilter.gameObject.GetComponent<MeshRenderer>().enabled = false;
 
 
                 Mesh mesh = new Mesh();
                 mesh.name = "Unanite";
                 mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
                 mesh.CombineMeshes(combine);
-
 
                 unanite.GetComponent<MeshFilter>().sharedMesh = mesh;
                 unanite.GetComponent<MeshRenderer>().material = nanite.meshFilter.GetComponent<MeshRenderer>().material;
